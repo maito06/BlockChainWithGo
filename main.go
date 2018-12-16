56cf417ea6535c79b64c07ac2f17f5b44419ba37
maito06 <lucazelmans@yahoo.fr>
2018-12-16 23:05:21 +0100

    Implementatiob of function calculateHash

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
 
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func main() {
+	var blockChain []Block
 }
